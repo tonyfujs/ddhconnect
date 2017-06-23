e1360d2cf3faa62f8da20099cafd5a7f51b991df
Tony Fujs <tony.fujs@gmail.com>
2017-06-23 16:21:39 -0400

    add package data

---
+++ R/data-attach_resources_template.R
@@ -0,0 +1,9 @@
+#' template to attach resource(s) to a dataset
+#'
+#' Small template to generate the correct JSON file to attach resource(s) to an existing dataset
+#'
+#' @format A list of length 1.
+#'
+#' @examples
+#' attach_resources_template
+"attach_resources_template"
