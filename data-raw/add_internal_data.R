9866773466875814d90a7bc455ff0c71fc9741ae
Tony Fujs <tony.fujs@gmail.com>
2017-07-27 12:54:28 -0400

    update package data

--- data-raw/add_internal_data.R
+++ data-raw/add_internal_data.R
@@ -5,11 +5,16 @@ attach_resources_template <- fromJSON('./data-raw/attach_single_resource_schema.
 production_root_url <- 'https://newdatacatalog.worldbank.org'
 stg_root_url <- 'https://datacatalogbetastg.worldbank.org/'
 
+test_dataset_update_json <- fromJSON('data-raw/test_dataset_update.json', simplifyVector = FALSE)
+test_resource_update_json <- fromJSON('data-raw/test_resource_update.json', simplifyVector = FALSE)
+
 # Save data
 devtools::use_data(attach_resources_template,
                   overwrite = TRUE)
 
 devtools::use_data(production_root_url,
                    stg_root_url,
+                   test_dataset_update_json,
+                   test_resource_update_json,
                    overwrite = TRUE,
                    internal = TRUE)
