d4b8e1b8008263283e00b10c25259b970052f408
meera <mbdesai@umich.edu>
2017-11-29 09:53:06 -0500

    save lookup tables

--- data-raw/add_internal_data.R
+++ data-raw/add_internal_data.R
@@ -2,7 +2,7 @@ library(jsonlite)
 
 attach_resources_template <- fromJSON('./data-raw/attach_single_resource_schema.json', simplifyVector = FALSE)
 
-production_root_url <- 'https://newdatacatalog.worldbank.org'
+production_root_url <- 'https://datacatalog.worldbank.org'
 stg_root_url <- 'https://newdatacatalogstg.worldbank.org/'
 
 test_dataset_update_json <- fromJSON('data-raw/test_dataset_update.json', simplifyVector = FALSE)
@@ -10,10 +10,15 @@ test_resource_update_json <- fromJSON('data-raw/test_resource_update.json', simp
 
 mandatory_text_fields <- readLines('data-raw/mandatory_text_fields.txt')
 
+dataset_json <- read.csv("./data-raw/field_names_dataset.csv")
+resource_json <- read.csv("./data-raw/field_names_resource.csv")
+
 # Save data
 devtools::use_data(attach_resources_template,
                    test_dataset_update_json,
                    test_resource_update_json,
+                   dataset_json,
+                   resource_json,
                    overwrite = TRUE)
 
 devtools::use_data(production_root_url,
