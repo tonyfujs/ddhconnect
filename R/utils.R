f6375f97521f39179bda48c46656f6eef635ee59
Tony Fujs <tony.fujs@gmail.com>
2017-11-30 14:46:11 -0500

    Fix dkanr namespace reference

--- R/utils.R
+++ R/utils.R
@@ -12,7 +12,7 @@ build_search_query <- function(fields,
   limit_text <- paste0('limit=', limit)
   fields_text <- paste(fields, collapse = ',')
   fields_text <- paste0('fields=[,', fields_text, ',]')
-  filters_text <- dkanr::filters_to_text_query(filters, 'filter')
+  filters_text <- dkanr:::filters_to_text_query(filters, 'filter')
 
   out <- paste(limit_text, fields_text, filters_text, sep = '&')
 }
