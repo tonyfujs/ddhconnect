8fe5af03eca35f3a224b850f52b3a3ccabe3485e
meera <mbdesai@umich.edu>
2018-01-16 16:12:52 -0500

    lint_package

--- R/logout_ddh.R
+++ R/logout_ddh.R
@@ -1,26 +1,29 @@
-#' logout_ddh
-#'
-#' Close an existing connection with the DDH API
-#'
-#' @param credentials list: authentication token and cookie
-#' @param root_url character: Server's root URL
-#'
-#' @return list
-#' @export
-#'
+#" logout_ddh
+#"
+#" Close an existing connection with the DDH API
+#"
+#" @param credentials list: authentication token and cookie
+#" @param root_url character: Server"s root URL
+#"
+#" @return list
+#" @export
+#"
 
-logout_ddh <- function(credentials = list(cookie = dkanr::get_cookie(), token = dkanr::get_token()), root_url = dkanr::get_url()) {
+logout_ddh <- function(credentials = list(cookie = dkanr::get_cookie(),
+                                          token = dkanr::get_token()),
+                       root_url = dkanr::get_url()) {
+
   cookie <- credentials$cookie
   token <- credentials$token
 
   # Build url
-  path <- 'api/dataset/user/logout'
+  path <- "api/dataset/user/logout"
   url <- httr::modify_url(root_url, path = path)
 
   out <- httr::POST(url,
-                    httr::add_headers(.headers = c('Content-Type' = 'application/json',
-                                                   'Cookie' =  cookie,
-                                                   'X-CSRF-Token' = token)),
+                    httr::add_headers(.headers = c("Content-Type" = "application/json",
+                                                   "Cookie" =  cookie,
+                                                   "X-CSRF-Token" = token)),
                     encode = "json")
   dkanr:::err_handler(out)
   out <- httr::content(out)
--- R/search_catalog.R
+++ R/search_catalog.R
@@ -1,19 +1,19 @@
-#' search_catalog
-#'
-#' Helper function to search the DDH data catalog
-#'
-#' @param fields character vector: fields to be returned by the search
-#' @param filters named character vector: filters to be applied to the search
-#' @param limit numeric: Maximum number of records to retrieve by search iteration
-#' @param root_url character: API root URL
-#' @param credentials list: API authentication credentials
-#'
-#' @return list
-#' @export
-#'
+#" search_catalog
+#"
+#" Helper function to search the DDH data catalog
+#"
+#" @param fields character vector: fields to be returned by the search
+#" @param filters named character vector: filters to be applied to the search
+#" @param limit numeric: Maximum number of records to retrieve by search iteration
+#" @param root_url character: API root URL
+#" @param credentials list: API authentication credentials
+#"
+#" @return list
+#" @export
+#"
 
-search_catalog <- function(fields = c('nid', 'uuid', 'title', 'field_contact_email', 'field_wbddh_data_type', 'status'),
-                           filters = c('field_wbddh_data_type'=294, 'status'=1),
+search_catalog <- function(fields = c("nid", "uuid", "title", "field_contact_email", "field_wbddh_data_type", "status"),
+                           filters = c("field_wbddh_data_type" = 294, "status" = 1),
                            limit = 200,
                            root_url = dkanr::get_url(),
                            credentials = list(cookie = dkanr::get_cookie(), token = dkanr::get_token())) {
@@ -35,12 +35,12 @@ search_catalog <- function(fields = c('nid', 'uuid', 'title', 'field_contact_ema
 
   # Return query
   iterations <- ceiling(count / limit)
-  out <- vector(mode = 'list', length = count)
+  out <- vector(mode = "list", length = count)
 
-  if (length(out) > 0){
+  if (length(out) > 0) {
     for (i in 1:iterations) {
       temp_offset <- (i - 1) * limit
-      temp_query <- paste0(query, '&offset=', temp_offset)
+      temp_query <- paste0(query, "&offset=", temp_offset)
       temp_resp <- search_ddh(credentials = credentials,
                               query = temp_query,
                               root_url = root_url)
@@ -48,7 +48,7 @@ search_catalog <- function(fields = c('nid', 'uuid', 'title', 'field_contact_ema
       index <- (1 + temp_offset):(temp_offset + length(temp_resp))
       out[index] <- purrr::map(temp_resp, function(x) x)
     }
-  }else {
+  } else {
     warning("Your query returned no results. Please try again.")
   }
 
