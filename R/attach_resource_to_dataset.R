96c976e6c584e6fb732d7215f4ef3f15ef3c827d
drkarthi <karthikthegoth@gmail.com>
2018-02-15 09:52:58 -0500

    handle errors on successful attachment

--- R/attach_resource_to_dataset.R
+++ R/attach_resource_to_dataset.R
@@ -1,6 +1,6 @@
-#' attach_resource_to_dataset
+#' attach_resources_to_dataset
 #'
-#' Attach resource to a dataset in DDH
+#' Attach resources to a dataset in DDH
 #'
 #' @param dataset_nid character: node id of the dataset to which the resources are to be attached
 #' @param resource_nids character vector: node ids of the resources to be attached
@@ -11,15 +11,22 @@
 #' @export
 #'
 
-attach_resource_to_dataset <- function(dataset_nid,
+attach_resources_to_dataset <- function(dataset_nid,
                                        resource_nids,
                                        root_url = dkanr::get_url(),
                                        credentials = list(cookie = dkanr::get_cookie(),
                                                           token = dkanr::get_token())) {
 
   body <- create_json_attach(resource_nids = resource_nids)
-  out <- dkanr::update_node(nid = dataset_nid, url = root_url,
-                            body = body, credentials = credentials)
+  # purrr::map(resource_nids, dkanr::update_node(nid = dataset_nid,
+  #                                              body = body))
+  for(i in 1:length(resource_nids)) {
+    tryCatch( {
+      dkanr::update_node(nid = dataset_nid, body = body)
+    }, error = function(e) {
+      print("Returns an error")
+    })
+  }
 
-  return(jsonlite::fromJSON(out))
+  # return(jsonlite::fromJSON(out))
 }
