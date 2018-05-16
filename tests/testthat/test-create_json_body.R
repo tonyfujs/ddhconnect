378946873aeb8501dc80282722aed6aaae38afec
drkarthi <karthikthegoth@gmail.com>
2018-05-16 12:53:14 -0400

    add test for create_json_body

--- tests/testthat/test-create_json_body.R
+++ tests/testthat/test-create_json_body.R
@@ -1,21 +1,22 @@
 context("test-create_json_body.R")
 root_url <- "https://datacatalog.worldbank.org"
 
+# start_capturing(path = './tests/testthat')
+# get_lovs()
+# stop_capturing()
+
 test_that("title field update works", {
-  body <- create_json_body(values = c("title" = "Test Create JSON"),
-                           node_type = "dataset",
-                           root_url = root_url)
+
+  body <- create_json_body(c("title"="Test Create JSON"), node_type = "resource")
   json_template <- list()
   json_template$title <- "Test Create JSON"
-  json_template$type <- "dataset"
+  json_template$type <- "resource"
   json_string <- jsonlite::toJSON(json_template, auto_unbox = TRUE)
   expect_equal(body, json_string)
 })
 
 test_that("text field update works", {
-  body <- create_json_body(values = c("body" = "Test Body"),
-                          node_type = "dataset",
-                          root_url = root_url)
+  body <- create_json_body(c("body"="Test Body"), node_type = "dataset")
   json_template <- list()
   json_template$body$und <- vector("list", length = 1)
   json_template$body$und[[1]]$value <- "Test Body"
@@ -23,17 +24,15 @@ test_that("text field update works", {
   json_string <- jsonlite::toJSON(json_template, auto_unbox = TRUE)
   expect_equal(body, json_string)
 })
-# TID fields require call to get_lovs()
-# test_that("tid field update works", {
-#   body <- create_json_body(values = c("field_topic" = "366"),
-#                           node_type = "dataset",
-#                           root_url = root_url)
-#   json_template <- list()
-#   json_template$field_topic$und <- list("366")
-#   json_template$type <- "dataset"
-#   json_string <- jsonlite::toJSON(json_template, auto_unbox = TRUE)
-#   expect_equal(body, json_string)
-# })
+
+test_that("tid field update works", {
+  body <- create_json_body(c("field_topic"="Energy and Extractives"), node_type = "dataset")
+  json_template <- list()
+  json_template$field_topic$und <- list("366")
+  json_template$type <- "dataset"
+  json_string <- jsonlite::toJSON(json_template, auto_unbox = TRUE)
+  expect_equal(body, json_string)
+})
 
 test_that("upi field update works", {
   body <- create_json_body(values = c("field_wbddh_dsttl_upi" = "46404"),
@@ -45,20 +44,17 @@ test_that("upi field update works", {
   json_string <- jsonlite::toJSON(json_template, auto_unbox = TRUE)
   expect_equal(body, json_string)
 })
-# TID fields require call to get_lovs()
-# test_that("multiple values update works", {
-#   body <- create_json_body(values = c("title" = "Test Create JSON",
-#                                      "body" = "Test Body",
-#                                      "field_topic" = "366",
-#                                      "field_wbddh_dsttl_upi" = "46404"),
-#                           node_type = "dataset",
-#                           root_url = root_url)
-#   json_template <- list()
-#   json_template$title <- "Test Create JSON"
-#   json_template$body$und[[1]]$value <- "Test Body"
-#   json_template$field_topic$und <- list("366")
-#   json_template$field_wbddh_dsttl_upi$und$autocomplete_hidden_value <- "46404"
-#   json_template$type <- "dataset"
-#   json_string <- jsonlite::toJSON(json_template, auto_unbox = TRUE)
-#   expect_equal(body, json_string)
-# })
+
+test_that("multiple tid values update works", {
+  body = create_json_body(c("title"="Test Create JSON", "body"="Test Body",
+                            "field_topic"="Energy and Extractives", "field_wbddh_dsttl_upi"="46404"),
+                          node_type = "dataset")
+  json_template <- list()
+  json_template$title <- "Test Create JSON"
+  json_template$body$und[[1]]$value <- "Test Body"
+  json_template$field_topic$und <- list("366")
+  json_template$field_wbddh_dsttl_upi$und$autocomplete_hidden_value <- "46404"
+  json_template$type <- "dataset"
+  json_string <- jsonlite::toJSON(json_template, auto_unbox = TRUE)
+  expect_equal(body, json_string)
+})
