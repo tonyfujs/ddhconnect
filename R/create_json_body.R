0cb5aff2207c0c00093828b00a6d3153e6852a7f
seladore <mbdesai@umich.edu>
2018-11-16 14:24:18 -0500

    split func for different nodes

--- R/create_json_body.R
+++ R/create_json_body.R
@@ -3,7 +3,7 @@
 #' Create the JSON body for updating the given fields or creating a new dataset
 #'
 #' @param values list: list of corresponding values that need to be updated
-#' @param node_type string: type of node to upload (dataset or resource)
+#' @param json_formats dataframe: lookup table for machine name and expected value (dataset or resource)
 #' @param root_url string: API root URL
 #'
 #' @import dplyr
@@ -12,76 +12,40 @@
 #'
 
 create_json_body <- function(values = list("title" = "Test Create JSON",
-                                          "body" = "Test Creation of JSON",
-                                          "field_wbddh_dsttl_upi" = "123",
-                                          "field_topic" = "Poverty",
-                                          "field_wbddh_country" = c("Antigua and Barbuda","Armenia")),
-                             node_type = "dataset",
+                                          "body" = "Test Creation of JSON"),
+                             json_formats = ddhconnect::dataset_json_format_lookup,
                              root_url = dkanr::get_url()) {
-  # check for valid field names
-  values_fields <- names(values)
-  valid_fields <- unique(get_fields(root_url)$machine_name)
-  valid_fields <- c(valid_fields, "workflow_status", "type")
-  invalid_fields <- setdiff(values_fields, valid_fields)
-  if(length(invalid_fields) > 0) {
-    stop(paste0("Invalid fields: ", paste(invalid_fields, collapse = "\n"),
-                "\nPlease choose a valid field from:\n",
-                paste(valid_fields, collapse = "\n")))
-  }
 
-  # get the correct JSON formats from the lookup table
-  json_body <- list()
-  if (node_type == "dataset") {
-    json_formats <- ddhconnect::dataset_json_format_lookup
-  } else if (node_type == "resource") {
-    json_formats <- ddhconnect::resource_json_format_lookup
-  }
-  else {
-    stop("Invalid value for node_type.
-         node_type must either be \"dataset\" or \"resource\".")
-  }
-  values["type"] <- node_type
-
-  # convert controlled vocabulary values into tid values
   values <- map_tids(values, root_url)
 
   machine_names <- json_formats$machine_names
   to_update <- subset(json_formats, machine_names %in% names(values))
+  json_body <- list()
   for (i in 1:nrow(to_update)) {
     field_name <- to_update[i, 1]
     json_template <- jsonlite::fromJSON(to_update[i, 2])
-    # title and status
     if (is.character(json_template[[field_name]])) {
+      # title and status
       json_template[[field_name]] <- safe_unbox(
                                        safe_assign(values[[field_name]])
                                      )
-    }
-    # controlled vocabulary fields
-    else if (is.null(names(json_template[[field_name]]$und))) {
+    } else if (is.null(names(json_template[[field_name]]$und))) {
+      # controlled vocabulary fields
       vals <- values[[field_name]]
-      # check for invalid values - not required any more since map_tids does the mapping
-      # lovs <- get_lovs(root_url)
-      # if (nrow(lovs[lovs$machine_name == field_name & lovs$tid %in% vals, ]) != length(vals)){
-      #   invalid_vals <- setdiff(vals,
-      #                           lovs[lovs$machine_name == field_name &
-      #                                lovs$tid %in% vals,
-      #                                "tid"])
-      #   stop(paste0("Invalid values for ", field_name, ": ",
-      #              paste(invalid_vals, collapse = " "),
-      #              "\nPlease choose from the valid values for ", field_name,
-      #              ":\n",
-      #              paste(lovs[lovs$machine_name == field_name, "tid"],
-      #                    collapse = ", ")))
-      # }
       json_template[[field_name]]$und <- vals
-    }
-    # free text fields
-    else {
+    } else {
+      # free text fields
       subfield_name <- names(json_template[[field_name]]$und)
       json_template[[field_name]]$und[[subfield_name]] <-
                                   safe_unbox(safe_assign(values[[field_name]]))
     }
-    json_body <- c(json_body, json_template)
+    json_body[field_name] <- json_template
   }
   return(jsonlite::toJSON(json_body))
+}
+
+format_tags <- function(value_string) {
+  vals <- unlist(strsplit(test_string, split = ";"))
+  out <- paste0('\"\"', vals, '\"\"', collapse = " ")
+  return(out)
 }
---
+++ R/create_json_resource.R
@@ -0,0 +1,43 @@
+#' create_json_resource
+#'
+#' Create the JSON body for updating the given fields or creating a new dataset
+#'
+#' @param values list: list of corresponding values that need to be updated
+#' @param node_type string: type of node to upload (dataset or resource)
+#' @param root_url string: API root URL
+#'
+#' @import dplyr
+#' @return json string
+#' @export
+#'
+
+create_json_resource <- function(values = list("title" = "Test Create JSON",
+                                          "body" = "Test Creation of JSON",
+                                          "field_wbddh_dsttl_upi" = "123",
+                                          "field_topic" = "Poverty",
+                                          "field_wbddh_country" = c("Antigua and Barbuda","Armenia")),
+                                workflow_status = "published",
+                                ddh_fields = NULL,
+                                root_url = dkanr::get_url()) {
+
+  if (missing(ddh_fields)) {
+    ddh_fields <- dkanr::get_fields(root_url)
+  }
+
+  values_fields <- names(values)
+  valid_fields <- unique(ddh_fields$machine_name[ddh_fields$node_type == "resource"])
+  invalid_fields <- setdiff(values_fields, valid_fields)
+  if (length(invalid_fields) > 0) {
+    stop(paste0("Invalid fields: ", paste(invalid_fields, collapse = "\n"),
+    "\nPlease choose a valid field from:\n",
+    paste(valid_fields, collapse = "\n")))
+  }
+
+  values["type"] <- "resource"
+  values["workflow_status"] <- workflow_status
+
+  # get the correct JSON formats from the lookup table
+  json_formats <- ddhconnect::resource_json_format_lookup
+  json_body <- create_json_body(values, json_formats, root_url)
+  return(jsonlite::toJSON(json_body))
+}
