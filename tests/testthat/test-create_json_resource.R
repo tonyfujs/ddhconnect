a969c1aa8aa374d6734f206e4c52dcfcd7bc101a
Alp Arslan Aziz <aaziz3@worldbank.org>
2019-01-17 20:43:24 -0500

    need to fix remaing 3 tests which are failing

--- tests/testthat/test-create_json_resource.R
+++ tests/testthat/test-create_json_resource.R
@@ -13,6 +13,16 @@ publication_status <- "published"
 # httptest::stop_capturing()
 
 test_that("basic resource json body builds correctly", {
+  # body <- create_json_resource(values = list("title" = "Test Resource Title",
+  #                                            "body" = "Test Resource Body",
+  #                                            "field_wbddh_data_class" = "Public",
+  #                                            "field_wbddh_resource_type" = "Resource Type not specified",
+  #                                            "field_format" = "Format Not Specified",
+  #                                            "field_link_api" = "www.google.com",
+  #                                            "field_ddh_harvest_src" = "Finances",
+  #                                            "field_ddh_harvest_sys_id" = "8675309"),
+  #                             publication_status, ddh_fields, lovs, root_url)
+  
   body <- create_json_resource(values = list("title" = "Test Resource Title",
                                              "body" = "Test Resource Body",
                                              "field_wbddh_data_class" = "Public",
@@ -20,19 +30,21 @@ test_that("basic resource json body builds correctly", {
                                              "field_format" = "Format Not Specified",
                                              "field_link_api" = "www.google.com",
                                              "field_ddh_harvest_src" = "Finances",
-                                             "field_ddh_harvest_sys_id" = "8675309"),
-                              publication_status, ddh_fields, lovs, root_url)
+                                             "field_ddh_harvest_sys_id" = "8675309"))
+  
   json_template <- list()
   json_template$title <- "Test Resource Title"
   json_template$body$und[[1]]$value <- "Test Resource Body"
-  json_template$field_wbddh_data_class$und$tid <- "358"
-  json_template$field_wbddh_resource_type$und$tid <- "877"
-  json_template$field_link_api$und[[1]]$url <- "www.google.com"
-  json_template$type <- "resource"
-  json_template$field_format$und$tid <- "1271"
-  json_template$field_ddh_harvest_src$und$tid <- "1015"
+  json_template$field_dataset_ref$und <- list(list("target_id" = "111"))
+  json_template$field_ddh_harvest_src$und <- list(list("tid" = "1015"))
   json_template$field_ddh_harvest_sys_id$und[[1]]$value <- "8675309"
-  json_template$workflow_status <- "published"
+  json_template$field_format$und <- list(list("tid" = "1271"))
+  json_template$field_link_api$und[[1]]$url <- "www.google.com"
+  # json_template$field_wbddh_data_class$und$tid <- "358"
+  json_template$field_wbddh_data_class$und <- list(list("tid" = "358"))
+  json_template$field_wbddh_resource_type$und <- list(list("tid" = "877"))
+  json_template$type <- "resource"
+  json_template$moderation_next_state <- "published"
   json_string <- jsonlite::toJSON(json_template, auto_unbox = TRUE)
   expect_equal(body, json_string)
 })
@@ -41,14 +53,14 @@ test_that("basic resource json body builds correctly with unpublished", {
   body <- create_json_resource(values = list("title" = "Test Resource Title",
                                              "body" = "Test Resource Body",
                                              "field_wbddh_data_class" = "Public"),
-                              publication_status = "unpublished",
-                              ddh_fields, lovs, root_url)
+                              publication_status = "unpublished")
   json_template <- list()
   json_template$title <- "Test Resource Title"
   json_template$body$und[[1]]$value <- "Test Resource Body"
-  json_template$field_wbddh_data_class$und$tid <- "358"
+  json_template$field_dataset_ref$und <- list(list("target_id" = "111"))
+  json_template$field_wbddh_data_class$und <- list(list("tid" = "358"))
   json_template$type <- "resource"
-  json_template$workflow_status <- "unpublished"
+  json_template$moderation_next_state <- "unpublished"
   json_string <- jsonlite::toJSON(json_template, auto_unbox = TRUE)
   expect_equal(body, json_string)
 })
@@ -61,8 +73,7 @@ test_that("tid fields update fails well for invalid field names", {
                       paste(invalid_fields, collapse = "\n"),
                       "\nPlease choose a valid field from:\n",
                       paste(fields, collapse = "\n"))
-  expect_error(create_json_resource(list("field_invalid_test" = c("Energy and Extractives", "Topic123")),
-                                   publication_status, ddh_fields, lovs, root_url),
+  expect_error(create_json_resource(list("field_invalid_test" = c("Energy and Extractives", "Topic123"))),
                paste0(error_msg, ".*"))
 })
 
@@ -70,8 +81,7 @@ test_that("tid fields update fails well for invalid values", {
   list_value_names <- lovs$list_value_name[lovs$machine_name == "field_wbddh_data_class"]
   error_msg <- paste0("Invalid value for field_wbddh_data_class. The valid values are:\n",
                       paste(list_value_names, collapse = "\n"))
-  expect_error(create_json_resource(list("field_wbddh_data_class" = c("Energy and Extractives", "Topic123")),
-                                   publication_status, ddh_fields, lovs, root_url),
+  expect_error(create_json_resource(list("field_wbddh_data_class" = c("Energy and Extractives", "Topic123"))),
                paste0(error_msg, ".*"))
 })
 
@@ -86,8 +96,7 @@ acquia_resource <- c(
 test_that("correct body for acquia", {
   body <- create_json_resource(values = acquia_resource,
                                publication_status,
-                               dataset_nid = "159981",
-                               ddh_fields, lovs, root_url)
+                               dataset_nid = "159981")
   # json_template <- list()
   # json_template$title <- "Test Create JSON"
   # json_template$field_topic$und <- list("366", "376")
